edcb2c5b606fe0009f7fd3b7aae495ec3be5a72e
jonas.ickert <@P8i@K!qJ4JgRK7bdoH8J4>
2024-06-28 15:16:43 +0200

    wrote RewardClipper, Preprocessing, FrameStack

--- Information/wrapper_info.md
+++ Information/wrapper_info.md
@@ -15,3 +15,24 @@ was sind Wrapper?
     - ***class*** gymnasium.wrappers.**AtariPreprocessing(*env: Env, *noop_max: int = 30*, *frame_skip: int = 4*, *screen_size: int = 84*, *terminal_on_life_loss: bool = False*, *grayscale_obs: bool = True*, *grayscale_newaxis: bool = False*, *scale_obs: bool = False*)**
 - frame stacking
     - ***class*** gymnasium.wrappers.**FrameStack(*env: Env, *num_stack: int*, *lz4_compress: bool = False)***
+
+
+### Grundsätzliche Idee der File (28.06, Jonas)
+
+---
+
+- 2 Möglichkeiten:
+  - File besteht aus 3 methoden die jeweils das Detail der übergeben 
+  bekommenen Environment "ändert"
+  - File besteht aus einer Klasse, die bei Erstellung eine Environment 
+  übergeben bekommt und die Environment nach allen Veränderungen 
+  zurückgibt
+- reward recht einfach, TransformReward()
+- preprocessing schwieriger, viele parameter unerklärt.
+  - kein frame skipping da frame stack = 4,
+  - oder frame skipping = 4 da frame stack = 4 und 4 frames das gleiche 
+  frame da ist, deswegen den frame skippen?
+  - *** atariPreprocessing nicht sinnvoll da von 210x180 auf 84x84 gescaled wird.
+  - scaling muss selber passieren => sleber schreiben
+  - greyscale ebenfalls selber schreiben.
+- Framestacing recht einfach, FrameStack.
---
+++ wrapper.py
@@ -0,0 +1,72 @@
+import gymnasium as gym
+from PIL import Image
+
+class wrapper():
+    """class wrapper contains the conversion of pixel observation to new parameters"""
+
+    class RewardClipper(gym.RewardWrapper):
+        """class for reard clipping 1.a"""
+        def __init__(self, env):
+            """:param env: environment where the reward should be clipped"""
+            super().__init__(env)
+
+        def reward(self, reward: float):
+            """
+            :param reward: the reward that should be clipped between -1 and 1
+            :return: is reward < -1 it's clipped to -1 (max), if reward > 1 it's clipped to 1 (min)
+            """
+            return min(max(-1, reward), 1)
+
+    class Preprocessing(gym.Wrapper):
+        """
+        Wrapper has a method atariPreprocessing, but it only converts atari frames witch 210x180 pixel.
+        Own preprocessing necessary because our games are not in 210x180
+        """
+        def __init__(self, env):
+            """:param env: environment where the reward should be clipped"""
+            super().__init__(env)
+
+        def reset(self):
+            """
+            Takes the normal observation from env.
+            Return of reset() in env is a np.array and not an image => call preprocess()
+            :return: loock return preprocess()
+            """
+            norm_obs = self.env.reset()
+            new_obs = self.preprocess(norm_obs[0])
+            return new_obs
+
+        def step(self, action):
+            """
+            Takes the normal observation from env.
+            Return of step() in env is also a np.array and not an image => call preprocess()
+            :return: loock return preprocess()
+            """
+            norm_obs, rew, done, info = self.env.step(action)
+            new_obs = self.preprocess(norm_obs[0])
+            return new_obs
+
+
+        def preprocess(self, obs_array):
+            """
+            :param obs_array: np.array to convert into an grayscale image
+            first, array has to be converted into image and than into grayscale
+            :return: new grayscale image
+            """
+            img = Image.fromarray(obs_array)
+            gray_image = img.convert("L")
+            return gray_image
+
+    def __init__(self, envi: gym.Env):
+        """:param envi: environmnet that has to be changed"""
+        self.env = self.RewardClipper(envi)
+        self.env = self.Preprocessing(self.env)
+        self.env = gym.wrappers.FrameStack(self.env, 4)
+        self.return_env()
+
+    def return_env(self):
+        """
+        funciton to return changed env, not possible in __init__()
+        :return: env with changed parameters
+        """
+        return self.env

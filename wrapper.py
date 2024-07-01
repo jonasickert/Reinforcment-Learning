f34a3b2abc303e201d89f4a0be4a76e7966efd82
jonas.ickert <@P8i@K!qJ4JgRK7bdoH8J4>
2024-07-01 20:53:59 +0200

    removed step and reset, added observatiopn

--- wrapper.py
+++ wrapper.py
@@ -1,12 +1,15 @@
+import cv2
 import gymnasium as gym
 import numpy as np
 from PIL import Image
+
 
 class Wrapper():
     """class wrapper contains the conversion of pixel observation to new parameters"""
 
     class RewardClipper(gym.RewardWrapper):
         """class for reard clipping 1.a"""
+
         def __init__(self, env):
             """:param env: environment where the reward should be clipped"""
             super().__init__(env)
@@ -19,51 +22,35 @@ class Wrapper():
             """
             return min(max(-0.01, reward), 0.01)
 
-    class Preprocessing(gym.Wrapper):
+    class Preprocessing(gym.ObservationWrapper):
         """
         Wrapper has a method atariPreprocessing, but it only converts atari frames witch 210x180 pixel.
         Own preprocessing necessary because our games are not in 210x180
         """
+
         def __init__(self, env):
             """:param env: environment where the reward should be clipped"""
             super().__init__(env)
             print("init Preprocessing done!")
 
-        def reset(self):
+        def observation(self, obs):
             """
-            Takes the normal observation from env.
-            Return of reset() in env is a np.array and not an image => call preprocess()
-            :return: loock return preprocess()
+            geschrieben von Artur! erstetz die zuvor geschriebenen step und reset Methoden.
+            modifies the obsertvation. Caution, not reward terminated and info.
+            :param obs:observation to modify
+            :return: grayscale "image" as an array
             """
-            norm_obs, info = self.env.reset()
-            new_obs = self.preprocess(norm_obs)
-            return new_obs, info
-
-        def step(self, action):
-            """
-            Takes the normal observation from env.
-            Return of step() in env is also a np.array and not an image => call preprocess()
-            :return: loock return preprocess()
-            """
-            norm_obs, rew, terminated, info, done = self.env.step(action)
-            new_obs = self.preprocess(norm_obs)
-            return new_obs, rew, terminated, info, done
-
-        def preprocess(self, obs_array):
-            """
-            :param obs_array: np.array to convert into a grayscale image
-            first, array has to be converted into image and then into grayscale
-            :return: new grayscale image
-            """
-            gray_image = Image.fromarray(obs_array)
-            gray_image = gray_image.convert("L")
-            return np.asarray(gray_image)
+            if len(obs.shape) == 3 and obs.shape[2] == 3:
+                obs = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+            obs = cv2.resize(obs, (84, 110))
+            obs = obs[13:97, :]
+            return obs
 
     def __init__(self, envi: gym.Env):
         """:param envi: environmnet that has to be changed"""
         self.env = self.RewardClipper(envi)
         self.env = self.Preprocessing(self.env)
-        self.env = gym.wrappers.FrameStack(self.env, 1)
+        self.env = gym.wrappers.FrameStack(self.env, 4)
         print("init Wrapper done!")
 
     def get_env(self):
@@ -74,8 +61,8 @@ class Wrapper():
         return self.env
 
 
-#----------------Test--------------------
-env = gym.make("Hopper-v4", render_mode="rgb_array")
+# ----------------Test--------------------
+env = gym.make("Hopper-v4", render_mode='rgb_array')
 twopac = Wrapper(env)
 pic = Image.fromarray(env.reset()[0])
 pic.show()
@@ -87,4 +74,3 @@ for i in range(20):
     if x[2]:
         twopac.env.reset()
 print("finish")
-

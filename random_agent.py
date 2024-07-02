b8fa41b84639e862d62be4311081915ce9e86518
meriemjemai <meriem.jemai@tu-dortmund.de>
2024-07-02 12:47:10 +0200

    # a little test to auf2a

--- random_agent.py
+++ random_agent.py
@@ -6,3 +6,19 @@ class random_agent:
 
     def select_action(self, state):
         return self.action_space.sample()
+
+
+
+# A little test 
+if __name__ == "__main__":
+    env = gym.make("CartPole-v1")
+    agent = random_agent(env.action_space)
+    state, _ = env.reset()
+    done = False
+    step = 0
+
+    while not done:
+        action = agent.select_action(state)
+        state, reward, done, truncated, info = env.step(action)
+        print(f"Step {step}: Selected action: {action}")
+        step += 1

5b1a8566cb765b37a603e94b7eb0da1b14269fdd
meriemjemai <meriem.jemai@tu-dortmund.de>
2024-07-03 00:05:37 +0200

    #added doc to random_agent

--- random_agent.py
+++ random_agent.py
@@ -1,15 +1,37 @@
 import gymnasium as gym
 
 class random_agent:
+
+    """
+    Agent that selects actions randomly from the action space.
+
+    Attributes:
+        action_space (gym.Space): Action space of the environment.
+    """
+
     def __init__(self, action_space):
+        """
+        Initialize the agent.
+
+        Args:
+            action_space (gym.Space): Action space of the environment.
+        """
         self.action_space = action_space
 
     def select_action(self, state):
+        """
+        Select an action randomly from the action space.
+
+        Args:
+            state: Current state of the environment.
+        Returns:
+            int: Randomly selected action index.
+        """
         return self.action_space.sample()
 
 
 
-# A little test 
+#A little test
 if __name__ == "__main__":
     env = gym.make("CartPole-v1")
     agent = random_agent(env.action_space)

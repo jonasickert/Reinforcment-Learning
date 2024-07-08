06648962e726cbf4f14c1e6d9c8dd34eb044b7ca
meriemjemai <meriem.jemai@tu-dortmund.de>
2024-07-08 18:25:51 +0200

    #modified init in the class agent #modified select_action in the class agent

--- agent.py
+++ agent.py
@@ -3,21 +3,42 @@ import gymnasium as gym
 import torch
 
 class DQNAgent():
-
+    
+    #code von jonas 
+    """
     def __init__(self, env:gym.Env, input_dim, **kwargs):
         print("Wello Horld")
         # initialisiere Netzwerk
         self.dqn = networks.QFunction(input_dim=input_dim, output_dim=len(env.action_space.shape))
         if kwargs:
             self.load_network(kwargs)
+    """        
+    #code von Maryem: removed len and .shape to test on CartPole-v1
+    def __init__(self, env:gym.Env, input_dim, **kwargs):
+        print("Wello Horld")
+        # initialisiere Netzwerk
+        self.dqn = networks.QFunction(input_dim=input_dim, output_dim=env.action_space.n)
+        if kwargs:
+            self.load_network(kwargs)
 
-    def select_action(self, state):
+    #code von jonas 
+    #def select_action(self, state):
         """Idea: use predict
         :param state: state to predict the action to
         :return: the action the agent should choose
         """
-        return self.dqn(state)
-
+    #    return self.dqn(state)
+        
+    #code von Maryem:
+    def select_action(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)  # Convert state to tensor
+        
+        with torch.no_grad():
+            action_values = self.dqn(state)
+        
+        action = torch.argmax(action_values, dim=1).item()
+        
+        return action
 
     def compute_action_value(self, state, action):
         """Idea: predict the actions for the state, takes all outputs, search for

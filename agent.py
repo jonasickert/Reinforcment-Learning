d5157ef2680a52a993476f25209b40b4e7b57a6c
meriemjemai <meriem.jemai@tu-dortmund.de>
2024-07-09 10:18:36 +0200

    #added doc strings and comments to init and select action in the class agent

--- agent.py
+++ agent.py
@@ -15,6 +15,14 @@ class DQNAgent():
     """        
     #code von Maryem: removed len and .shape to test on CartPole-v1
     def __init__(self, env:gym.Env, input_dim, **kwargs):
+        """
+        Initializes the DQNAgent with the given environment and input dimension.
+
+        Parameters:
+        env (gym.Env): The environment in which the agent will operate.
+        input_dim (int): The dimension of the input (observation space).
+
+        """
         print("Wello Horld")
         # initialisiere Netzwerk
         self.dqn = networks.QFunction(input_dim=input_dim, output_dim=env.action_space.n)
@@ -31,11 +39,22 @@ class DQNAgent():
         
     #code von Maryem:
     def select_action(self, state):
+        """
+        Selects an action based on the given state using the Deep Q-Network (DQN).
+
+        Parameters:
+        state (numpy.ndarray): The current state of the environment represented as a NumPy array.
+
+        Returns:
+        int: The selected action, represented as an integer.
+        """
         state = torch.from_numpy(state).float().unsqueeze(0)  # Convert state to tensor
-        
+
+        # Disable gradient calculation since we are in evaluation mode
         with torch.no_grad():
             action_values = self.dqn(state)
         
+        # Select the action with the highest value
         action = torch.argmax(action_values, dim=1).item()
         
         return action

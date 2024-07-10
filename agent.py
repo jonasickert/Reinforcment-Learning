3da948da20d31dfed32b9f1a1079f262c6befe19
jonas.ickert <@P8i@K!qJ4JgRK7bdoH8J4>
2024-07-10 08:15:10 +0200

    agent

--- agent.py
+++ agent.py
@@ -4,15 +4,8 @@ import torch
 
 class DQNAgent():
     
-    #code von jonas 
-    """
-    def __init__(self, env:gym.Env, input_dim, **kwargs):
-        print("Wello Horld")
-        # initialisiere Netzwerk
-        self.dqn = networks.QFunction(input_dim=input_dim, output_dim=len(env.action_space.shape))
-        if kwargs:
-            self.load_network(kwargs)
-    """        
+
+    #code von Jonas
     #code von Maryem: removed len and .shape to test on CartPole-v1
     def __init__(self, env:gym.Env, input_dim, **kwargs):
         """
@@ -29,14 +22,6 @@ class DQNAgent():
         if kwargs:
             self.load_network(kwargs)
 
-    #code von jonas 
-    #def select_action(self, state):
-        """Idea: use predict
-        :param state: state to predict the action to
-        :return: the action the agent should choose
-        """
-    #    return self.dqn(state)
-        
     #code von Maryem:
     def select_action(self, state):
         """
@@ -58,16 +43,7 @@ class DQNAgent():
         action = torch.argmax(action_values, dim=1).item()
         
         return action
-    
-    #code von Jonas
-    #def compute_action_value(self, state, action):
-        """Idea: predict the actions for the state, takes all outputs, search for
-        the correct action and return it
-        :param state: stated that is chosen
-        :param action: action that is chosen
-        :return: the action value for specific state and action
-        """
-    #    print("Wello Horld")
+
 
     #code von Maryem for task 4)b
     def compute_action_value(self, state, action):
@@ -91,14 +67,14 @@ class DQNAgent():
         # Return the Q-value corresponding to the given action
         return action_values[action]
 
-
+    # code von Jonas
     def save_network(self, name):
         """ saves the network parameters as name
         :param name: of the save
         """
-        print("Mini mini man, DQN")
         torch.save(self.dqn.state_dict(), name)
 
+    # code von Jonas
     def load_network(self, name):
         """ load the network
         :param name: of the loaded network
